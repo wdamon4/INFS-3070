adc208e2c508d3ce84b053e68de0b1552613a90c
Wyatt Damon <Wyatt Damon@10.0.0.210>
2022-02-02 19:05:53 -0700

    Added examples of commenting in code and two more lines

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,19 @@
-
+/*
+ * Just my first program to 
+ * demonstrate how good I am 
+ * at programming in java with eclipse
+ */
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		
+		// Code to display message "Hello World AGAIN!" in console.
+		
 		System.out.println("Hello World AGAIN!");
+		
+		System.out.println("How are you doing today?");
+		
+		System.out.println("line");
 
 	}
 
