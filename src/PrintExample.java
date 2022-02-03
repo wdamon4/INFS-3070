31b6c9ef9452f8737eb43c2f5f60e8327d122b71
Wyatt Damon <Wyatt Damon@10.0.0.210>
2022-02-02 20:07:12 -0700

    Added print examples, but still needs work

---
+++ src/PrintExample.java
@@ -0,0 +1,12 @@
+
+public class PrintExample {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is my message");
+		System.out.println("This is my second message");
+		System.out.println("This is my third message");
+
+	}
+
+}
