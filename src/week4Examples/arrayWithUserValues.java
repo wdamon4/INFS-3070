bfb27856f853c20b6aa2212de1f9ad91c8895d3e
Wyatt Damon <Wyatt Damon@Wyatts-PC.uccs.edu>
2022-02-10 14:55:18 -0700

    Array Examples

---
+++ src/week4Examples/arrayWithUserValues.java
@@ -0,0 +1,36 @@
+package week4Examples;
+
+import java.util.Scanner;
+
+public class arrayWithUserValues {
+
+	public static void main(String[] args) {
+		
+		final int employees = 3; // Initial number of employees
+		int[] hours = new int[employees]; // Defining new array
+		
+		//Scanner Object
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Enter hours worked by " + employees + " employees.");
+		
+		// Employee 1 hours
+		System.out.println("Employee 1: ");
+		hours[0] = keyboard.nextInt();
+		
+		// Employee 2 Hours
+		System.out.println("Employee 2: ");
+		hours[1] = keyboard.nextInt();
+		
+		// Employee 3 hours
+		System.out.println("Employee 3: ");
+		hours[2] = keyboard.nextInt();
+		
+		// Display hours entered
+		System.out.println("The hours you entered are: ");
+		System.out.println(hours[0]);
+		System.out.println(hours[1]);
+		System.out.println(hours[2]);
+	}
+
+}
---
+++ src/week4Examples/basicArrayExample.java
@@ -0,0 +1,26 @@
+package week4Examples;
+
+public class basicArrayExample {
+
+	public static void main(String[] args) {
+		
+		/* Examples of basic array
+		final int numGrades = 4;
+		
+		int[] grades = new int[numGrades];
+		
+		grades[0] = 87;
+		grades[1] = 99;
+		grades[2] = 100;
+		grades[3] = 67;
+		
+		System.out.println(grades[0]);
+		*/
+		
+		// Easier method for basic array 
+		int[] grades = {87, 99, 100, 67}; 
+		
+		System.out.println(grades[2]);
+	}
+
+}
