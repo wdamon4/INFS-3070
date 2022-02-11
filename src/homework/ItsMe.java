37724e6d93b4247173cf6eac613568b2e3b5f0e1
Wyatt Damon <Wyatt Damon@10.0.0.210>
2022-02-11 15:14:01 -0700

    Homework #2

---
+++ src/homework/ItsMe.java
@@ -0,0 +1,25 @@
+package homework;
+
+public class ItsMe {
+
+	public static void main(String[] args) {
+		
+		// Creating 3 string variables with my information
+		String studentName = "Wyatt Damon";
+		String studentMajor = "Business Marketing major with an INFS minor";
+		String studentBirthPlace = "Fort Collins, CO"; 
+		
+		// Displaying each variable on a separate line 
+		System.out.println("My name is " + studentName);
+		System.out.println("I am a " + studentMajor);
+		System.out.println("I was born in " + studentBirthPlace);
+
+		// Creating two variables to multiply together, then display the result 
+		int variable1 = 5 ;
+		int variable2 = 8 ; 
+		int answer = variable1 * variable2 ;
+		
+		System.out.println(answer);
+	}
+
+}
