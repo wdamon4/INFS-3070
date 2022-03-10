b30d003bc507b30cd7b2dfbb84f33d187b842f1e
Wyatt Damon <Wyatt Damon@10.0.0.210>
2022-03-10 09:24:15 -0700

    Name Change

--- src/homework/MyInfo.java
+++ src/homework/MyInfo.java
@@ -4,7 +4,7 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class ItsMe {
+public class MyInfo {
 
 	public static void main(String[] args) {
 		
