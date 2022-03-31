93bf322ef22c12ace2ff8ddc096f72e4fe88dc73
Wyatt Damon <Wyatt Damon@Wyatts-PC.uccs.edu>
2022-03-31 15:06:56 -0600

    added on more

--- src/week10Examples/firstSwingUI.java
+++ src/week10Examples/firstSwingUI.java
@@ -9,6 +9,11 @@ public class firstSwingUI {
 		String name = JOptionPane.showInputDialog("Enter your name: ");
 		JOptionPane.showMessageDialog(null, "Hello " + name);
 		
+		int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age: "));
+		
+		// Convert input to integer
+		JOptionPane.showMessageDialog(null, "You are " + age + " years old.");
+		
 	}
 
 }
